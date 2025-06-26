d9146aeb6ce2b9cdc1bf092aa9e4db11d308e957
zeltolira <zelto16@hotmail.com>
2025-06-26 05:48:35 -0300

    configura rotas para balanceamento de carga

--- src/main/java/com/lira/mscloudgateway/MscloudgatewayApplication.java
+++ src/main/java/com/lira/mscloudgateway/MscloudgatewayApplication.java
@@ -2,12 +2,24 @@ package com.lira.mscloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MscloudgatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MscloudgatewayApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder){
+		return builder
+				.routes()
+					.route(r -> r.path("/produtos/**").uri("lb://produto-service"))
+				.build();
+	}
 }
